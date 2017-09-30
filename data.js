93113a103a53e61b07fbcac5c96d930714046859
Alex Bender <axbndr@gmail.com>
2017-09-30 15:28:09 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 {
-    "version": "",
+    "version": "U2F0IFNlcCAzMA==",
     "songs": [
         {"id": 1, "name": "", "url": "", "op": ""},
-        {"id": 2, "name": "", "url": "", "op": ""}
+        {"id": 2, "name": "", "url": "", "op": ""},
+        {"id": 3, "name": "Insects", "band": "brasstronaut", "url": "https://brasstronaut.bandcamp.com/track/insects", "op": ""}
     ]
 }
