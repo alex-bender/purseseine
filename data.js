029a3bb29341752deeac705139545f5b61f612c5
Alex Bender <axbndr@gmail.com>
2017-10-01 22:14:53 +0300

    Add Archive Kings of Speed

--- data.js
+++ data.js
@@ -4,5 +4,6 @@
         {"id": 1, "name": "", "url": "", "op": ""},
         {"id": 2, "name": "", "url": "", "op": ""},
         {"id": 3, "name": "Insects", "band": "brasstronaut", "url": "https://brasstronaut.bandcamp.com/track/insects", "op": ""}
+        {"id": 4, "name": "Kings Of Speed", "band": "archive", "url": "https://youtu.be/8N5KsXi5kVE", "op": ""}
     ]
 }
