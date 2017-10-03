7f32a69541cdbd0a78078d3357b0090852ccb882
Alex Bender <axbndr@gmail.com>
2017-10-03 18:46:01 +0300

    Add song

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
 {
-    "version": "U2F0IFNlcCAzMA==",
+    "version": "VHVlIE9jdCAwMw==",
     "songs": [
         {"id": 1, "name": "", "url": "", "op": ""},
         {"id": 2, "name": "", "url": "", "op": ""},
         {"id": 3, "name": "insects", "band": "brasstronaut", "url": "https://brasstronaut.bandcamp.com/track/insects", "op": ""}
         {"id": 4, "name": "kings of speed", "band": "archive", "url": "https://youtu.be/8N5KsXi5kVE", "op": ""}
         {"id": 5, "name": "the victorians are here", "band": "the secret love parade", "url": "https://youtu.be/lkemuRNF8m0", "op": ""}
+        {"id": 6, "name": "The Weatherman", "band": "Matt Holubowski", "url": "https://matt-holubowski.bandcamp.com/track/the-weatherman-or-the-felonies-are-magnets-on-my-coat", "op": ""}
     ]
 }
