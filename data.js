c8e6a9fb5a0fa56517c11d3b682bf0e59e3ab172
Alex Bender <axbndr@gmail.com>
2017-10-05 17:41:05 +0300

    Add song

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 {
-    "version": "V2VkIE9jdCAwNA==",
+    "version": "VGh1IE9jdCAwNQ==",
     "songs": [
         {"id": 1, "name": "", "url": "", "op": ""},
         {"id": 2, "name": "", "url": "", "op": ""},
@@ -7,6 +7,7 @@
         {"id": 4, "name": "kings of speed", "band": "archive", "url": "https://youtu.be/8N5KsXi5kVE", "op": ""},
         {"id": 5, "name": "the victorians are here", "band": "the secret love parade", "url": "https://youtu.be/lkemuRNF8m0", "op": ""},
         {"id": 6, "name": "The Weatherman", "band": "Matt Holubowski", "url": "https://matt-holubowski.bandcamp.com/track/the-weatherman-or-the-felonies-are-magnets-on-my-coat", "op": ""},
-        {"id": 7, "name": "Old world lies", "band": "brasstronaut", "url": "https://vimeo.com/6293927", "op": ""}
+        {"id": 7, "name": "Old world lies", "band": "brasstronaut", "url": "https://vimeo.com/6293927", "op": ""},
+        {"id": 8, "name": "Sugar man", "band": "Rodriguez", "url": "https://youtu.be/qyE9vFGKogs", "op": ""}
     ]
 }
